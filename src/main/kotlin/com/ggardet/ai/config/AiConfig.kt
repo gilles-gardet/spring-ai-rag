8867c471384daa70e9137ab685daa24fc4208cf5
Gilles <gilles.gardet@gmail.com>
2025-09-21 18:35:57 +0200

    feat: add advisor to augment answers with web search

--- src/main/kotlin/com/ggardet/ai/config/AiConfig.kt
+++ src/main/kotlin/com/ggardet/ai/config/AiConfig.kt
@@ -1,34 +1,69 @@
 package com.ggardet.ai.config
 
+import com.ggardet.ai.retriever.SearchEngineDocumentRetriever
 import org.springframework.ai.chat.client.ChatClient
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor
 import org.springframework.ai.chat.model.ChatModel
 import org.springframework.ai.chat.prompt.Prompt
 import org.springframework.ai.chat.prompt.SystemPromptTemplate
-import org.springframework.ai.vectorstore.SearchRequest
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor
+import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever
 import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.Resource
+import org.springframework.web.client.RestClient
 
 
 @Configuration
 class AiConfig {
-
     @Bean
     fun ragPrompt(@Value("classpath:/prompt.st") prompt: Resource): Prompt =
         SystemPromptTemplate(prompt).create()
 
     @Bean
-    fun chatClientBuilder(chatModel: ChatModel, vectorStore: VectorStore): ChatClient.Builder {
-        val searchRequest = SearchRequest.builder()
-            .similarityThreshold(0.8)
-            .topK(3)
+    fun chatClientBuilder(
+        chatModel: ChatModel,
+        @Qualifier("webRagAdvisor") webRagAdvisor: RetrievalAugmentationAdvisor,
+        @Qualifier("vectorRagAdvisor") vectorRagAdvisor: RetrievalAugmentationAdvisor,
+    ): ChatClient.Builder = ChatClient.builder(chatModel).defaultAdvisors(webRagAdvisor, vectorRagAdvisor)
+
+    @Bean("webRagAdvisor")
+    fun webRagAdvisor(
+        chatClientBuilder: ChatClient.Builder,
+        searchEngineDocumentRetriever: SearchEngineDocumentRetriever
+    ): RetrievalAugmentationAdvisor =
+        RetrievalAugmentationAdvisor.builder()
+            .queryTransformers(
+                RewriteQueryTransformer.builder()
+                    .chatClientBuilder(chatClientBuilder.clone())
+                    .targetSearchSystem("web search engine")
+                    .build()
+            )
+            .documentRetriever(searchEngineDocumentRetriever)
+            .queryAugmenter(
+                ContextualQueryAugmenter.builder()
+                    .allowEmptyContext(false)
+                    .build()
+            )
             .build()
-        val questionAnswerAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
-            .searchRequest(searchRequest)
+
+    @Bean("vectorRagAdvisor")
+    fun vectorRagAdvisor(vectorStore: VectorStore): RetrievalAugmentationAdvisor =
+        RetrievalAugmentationAdvisor.builder()
+            .documentRetriever(
+                VectorStoreDocumentRetriever.builder()
+                    .vectorStore(vectorStore)
+                    .similarityThreshold(0.5)
+                    .topK(3)
+                    .build()
+            )
             .build()
-        return ChatClient.builder(chatModel).defaultAdvisors(questionAnswerAdvisor)
-    }
+
+    @Bean
+    fun searchEngineDocumentRetriever(restClientBuilder: RestClient.Builder): SearchEngineDocumentRetriever =
+        SearchEngineDocumentRetriever(restClientBuilder, 10)
 }
---
+++ src/main/kotlin/com/ggardet/ai/retriever/SearchEngineDocumentRetriever.kt
@@ -0,0 +1,54 @@
+package com.ggardet.ai.retriever
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import org.springframework.ai.document.Document
+import org.springframework.ai.rag.Query
+import org.springframework.ai.rag.retrieval.search.DocumentRetriever
+import org.springframework.http.HttpHeaders
+import org.springframework.web.client.RestClient
+
+private const val URL_TAVILY_API = "https://api.tavily.com/search"
+
+class SearchEngineDocumentRetriever(
+    restClientBuilder: RestClient.Builder,
+    private val maxResults: Int
+) : DocumentRetriever {
+
+    private val restClient: RestClient = restClientBuilder
+        .baseUrl(URL_TAVILY_API)
+        .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer ${System.getenv("TAVILY_API_KEY")}")
+        .build()
+
+    override fun retrieve(query: Query): List<Document> {
+        val response = restClient.post()
+            .body(TavilySearchRequest(query.text(), "advanced", maxResults))
+            .retrieve()
+            .body(TavilySearchResponse::class.java)
+        return response?.results
+            ?.map { result ->
+                Document.builder()
+                    .text(result.content)
+                    .metadata("title", result.title ?: "No title")
+                    .metadata("url", result.url ?: "No url")
+                    .score(result.score)
+                    .build()
+            } ?: emptyList()
+    }
+
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+    internal data class TavilySearchRequest(
+        val query: String,
+        val searchDepth: String,
+        val maxResults: Int
+    )
+
+    internal data class TavilySearchResponse(val results: List<Result>?)
+
+    internal data class Result(
+        val title: String?,
+        val url: String?,
+        val content: String?,
+        val score: Double?
+    )
+}
